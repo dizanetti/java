7a764f404e1e47d6d326a0cc21f3daad23a0965f
Diego Zanetti <di.zanetti@outlook.com>
2022-04-26 17:33:33 +0100

    lab-rabbitmq

---
+++ lab-rabbitmq/src/main/java/com/labrabbitmq/LabRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.labrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabRabbitmqApplication.class, args);
+	}
+
+}
---
+++ lab-rabbitmq/src/test/java/com/labrabbitmq/LabRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
